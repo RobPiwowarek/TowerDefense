45986a44ea2e0e79fa92442bb7e0b3af09aaefe7
RobPiwowarek <robpiwowarek@gmail.com>
2016-04-21 10:39:41 +0200

    Update

---
+++ logic/Item.h
@@ -0,0 +1,21 @@
+#ifndef TOWERDEFENSE_ITEM_H
+#define TOWERDEFENSE_ITEM_H
+
+#include "Entity.h"
+
+namespace tower_defense{
+    class Item;
+}
+
+namespace tower_defense {
+    class Item : public Entity {
+
+    public:
+        Item(const tower_defense::Point &location, const double angle, const double size, const int itemClass);
+
+
+    private:
+    };
+}
+
+#endif
